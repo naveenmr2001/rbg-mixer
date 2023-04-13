7c19eca2a6b0320c4b902d5df5ba3058f602df59
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-04-13 10:47:59 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function rangeSlider(value)
+{
+  document.getElementById("rangeValue").innerHTML = value;
+}
+
+function rangeSlider2(value)
+{
+  document.getElementById("rangeValue-2").innerHTML = value;
+}
+
+function rangeSlider3(value)
+{
+  document.getElementById("rangeValue-3").innerHTML = value;
+}
+
+function rangeSlider4(value)
+{
+  document.getElementById("rangeValue-4").innerHTML = value;
+}
