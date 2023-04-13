f1cc94638f0b4229965ec844f9c8ee69c6f81d14
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-04-13 11:36:21 +0530

    Requirment1 Add. Added the full logic of the code

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ function rangeSlider4(value)
 {
   document.getElementById("rangeValue-4").innerHTML = value;
 }
+
+
+const redSlider = document.querySelector('.range');
+const greenSlider = document.querySelector('.range-2');
+const blueSlider = document.querySelector('.range-3');
+const colorPreview = document.querySelector('.main-container');
+const rgba = document.querySelector('.rgba-value');
+const header = document.querySelector('.header');
+console.log(redSlider)
+console.log(greenSlider)
+console.log(blueSlider)
+console.log(colorPreview)
+
+window.addEventListener("load", (event) => {
+    updateColor();
+  });
+
+function updateColor() {
+  const redValue = redSlider.value;
+  const greenValue = greenSlider.value;
+  const blueValue = blueSlider.value;
+
+  const color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+  console.log(color)
+  colorPreview.style.backgroundColor = color;
+  rgba.innerHTML = `rbg(${redValue}, ${greenValue}, ${blueValue})`;
+  rgba.style.color = color;
+  header.style.color = color;
+}
+
+redSlider.addEventListener('input', updateColor);
+greenSlider.addEventListener('input', updateColor);
+blueSlider.addEventListener('input', updateColor);
